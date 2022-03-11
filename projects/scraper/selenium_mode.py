5e52e35140435ccba7291e00b14be58ad6a9a115
yofikingstonp111 <yofikingston@gmail.com>
2022-03-12 02:15:42 +0700

    walmart-scrapers

--- projects/scraper/selenium_mode.py
+++ projects/scraper/selenium_mode.py
@@ -1,11 +1,31 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 website = "https://www.walmart.com/search?q=computer"
 
+def get_total_pages(keywords):
+    # used to search total pages
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    actions = ActionChains(driver)
+    next_page_btn = driver.find_element(By.XPATH, ('//li[@aria-label="false"]//a'))
+    actions.move_to_element(next_page_btn).perform()
+    time.sleep(0.5)
+    next_page_btn.click()
+    # get url
+    driver.get(website)
+
+    pages_source = driver.page_source
+    soup = BeautifulSoup(pages_source, 'html.parser')
+
+    pages =[]
 
 def get_all_items():
 
@@ -53,4 +73,5 @@ def get_all_items():
 
 
 if __name__ == '__main__':
+    get_total_pages()
     get_all_items()
