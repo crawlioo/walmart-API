0f295e76bbe8337b243ef6ad439c9f68091a036d
yofikingstonp111 <yofikingston@gmail.com>
2022-03-16 09:16:01 +0700

    walmart-scrapers

--- projects/scraper/selenium_mode.py
+++ projects/scraper/selenium_mode.py
@@ -6,27 +6,25 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 
-
-
 def get_total_pages(keypass):
     # used to search total pages
 
     website = f'https://www.walmart.com/search?q={keypass}'
     options = Options()
-    options.headless = False
+    options.headless = True
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     # get url
     driver.get(website)
-    #maximize_windows
+    # maximize_windows
     driver.maximize_window()
 
-    #pagination
+    # pagination
     pages_source = driver.page_source
 
     # temporary file
-    f = open("selenium_mode.html", "w+" )
+    f = open("selenium_mode.html", "w+")
     f.write(pages_source)
     f.close()
 
@@ -47,21 +45,22 @@ def get_total_pages(keypass):
     return total_pages
 
 
-def get_all_items(keypass):
-    pages = BeautifulSoup.find('')
+def get_all_items(keypass, pages):
     # pages = BeautifulSoup(, 'html.parser')
     # while pages.text.strip() == 'Robot or human?':
     #     # implement proxy
     #     keypass =
     #     soup = BeautifulSoup(res.text, 'html.parser')
 
-    website = f'https://www.walmart.com/search?q={keypass}'
+    website = f'https://www.walmart.com/search?q={keypass}&page={pages}&affinityOverride=default'
     options = Options()
     options.add_argument("start-maximized")
 
-    #avoiding detection
+    # avoiding detection
     options.add_argument('--disable-blink-features=AutomationControlled')
-    driver = webdriver.Chrome( options=options, service=Service (ChromeDriverManager().install()))
+    options.headless = True
+
+    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
     # get url
     driver.get(website)
@@ -69,42 +68,65 @@ def get_all_items(keypass):
     pages_source = driver.page_source
     soup = BeautifulSoup(pages_source, 'html.parser')
     product_list: list = []
-    #scraping proccess
-    headers_contents = soup.find('div', attrs={'class':'flex flex-wrap w-100 flex-grow-0 flex-shrink-0 ph2 pr0-xl pl4-xl mt0-xl mt3'})
-    contents = headers_contents.find_all('div', attrs={'data-testid':'list-view'})
+    # scraping proccess
+    headers_contents = soup.find('div', attrs={
+        'class': 'flex flex-wrap w-100 flex-grow-0 flex-shrink-0 ph2 pr0-xl pl4-xl mt0-xl mt3'})
+    contents = headers_contents.find_all('div', attrs={'data-testid': 'list-view'})
     for content in contents:
 
-        title = content.find('span', attrs={'class':'f6 f5-l normal dark-gray mb0 mt1 lh-title'}).text.strip()
+        title = content.find('span', attrs={'class': 'f6 f5-l normal dark-gray mb0 mt1 lh-title'}).text.strip()
         try:
             price = content.find('div', attrs={'class': 'b f5 f4-l black mr1 lh-copy'}).text.strip()
         except:
-            price = content.find('div', attrs={'class' :'b black f5 mr1 mr2-xl lh-copy f4-l', 'aria-hidden': 'true'}).text.strip()
+            try:
+                price = content.find('div', attrs={'aria-hidden': 'true'}).text.strip()
+            except:
+                try:
+                    price = content.find('ul', attrs={'list flex items-center justify-center pa0'}).text.strip()
+                except:
+                    price = content.find('li', attrs={'aria-hidden="false"'})
         link = soup.find('a', attrs={'class': 'absolute w-100 h-100 z-1'})['href']
         try:
-            rating = soup.find('div', attrs={'class': 'mt2 flex items-center'}).find('span', attrs={'class': 'w_A5'}).text.strip()
+            rating = soup.find('div', attrs={'class': 'mt2 flex items-center'}).find('span', attrs={
+                'class': 'w_A5'}).text.strip()
         except:
             rating = soup.find('span', attrs={'w_R'}).text.strip()
 
-        data_dict : dict ={
-            'title' : title,
+        data_dict: dict = {
+            'title': title,
             'price': price,
             'link': link,
             'rating': rating,
 
         }
+        print(data_dict)
         product_list.append(data_dict)
+
     return product_list
 
 
+def main(keypass):
+    final_result = []
+
+    total_pages = get_total_pages(keypass)
+    for page in range(total_pages):
+        page += 1
+        print(f'Scraping halaman ke:{page}')
+        products = get_all_items(keypass, page)
+        final_result += products
+    # proccess data here
+    total_data = len(final_result)
+
+    print('Ini adalah total halaman yang sudah di scrape '.format(total_data))
+
 
 if __name__ == '__main__':
+    keypass = 'computer'  # <bisa diganti input
+    main(keypass)
     # pages = []
     # data = ['', '1', '2', '3', '4', '...', '25', '']
-    get_total_pages()
-
 
     # get_pagination = []
     # for page in data:
     #     get_pagination.append(page[24])
     # print(get_pagination)
-
